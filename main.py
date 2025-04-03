6105e18a30b839af17b92a8ce1a68df7ca6e5ee3
katiaviene <katiacookiefairy@gmail.com>
2025-04-03 18:47:57 +0300

    better

--- main.py
+++ main.py
@@ -2,37 +2,21 @@ from collections import Counter
 import sys
 string = 'gogogopowerrangergogogopowerranger'
 pattern = 'xxxyxxxy'
-
-
-counts = dict(Counter(pattern))
-
-def get_all_substrings(s):
-    return [s[i:j] for i in range(len(s)) for j in range(i + 1, len(s) + 1) if s[i:j] in string]
-
-def match(liss):
-    if len(counts.keys())==len(liss.keys()):
-        collect_string = pattern.replace('x', liss['x']).replace('y', liss['y'])
-        if collect_string == string:
-            sys.exit(0)
-            return liss.values()
-    
-list = sorted(get_all_substrings(string))
-counts_sub = dict(Counter(list))
-
-liss = {}
-for sub in set(list):
-    for key, value in counts.items():
-        if counts_sub[sub] == counts[key]:
-            liss[key]=sub
-    match(liss)          
             
-for x in set(list):
-    for y in set(list):
-        if counts_sub[x] == counts['x'] and counts_sub[y] == counts['y']:
-            print(match({'x': x, 'y': y}))
-            
+def get_match(string, pattern):
+    counts = dict(Counter(pattern))
+    list_all_subscrings = [string[i:j] for i in range(len(string)) for j in range(i + 1, len(string) + 1) if string[i:j] in string]
+    counts_sub = dict(Counter(list_all_subscrings))
+    pair_match = {}
+    for x in set(list_all_subscrings):
+        for y in set(list_all_subscrings):
+            if counts_sub[x] == counts['x'] and counts_sub[y] == counts['y']:
+                pair_to_check = {'x': x, 'y': y}
+                if len(counts.keys())==len(pair_to_check.keys()):
+                    collect_string = pattern.replace('x', pair_to_check['x']).replace('y', pair_to_check['y'])
+                    if collect_string == string:
+                        pair_match = pair_to_check
+    return pair_match
 
-   
 
-
-    
+print(get_match(string, pattern))
