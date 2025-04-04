479b5aa5336c3afa647be0df689f34c636a2bf88
katiaviene <katiacookiefairy@gmail.com>
2025-04-04 08:20:18 +0300

    better

--- main.py
+++ main.py
@@ -1,27 +1,39 @@
 from collections import Counter
-from itertools import product
 
-string = 'gogogobb'
-pattern = 'yyyxx'
-
-# the order of result is always - x,y
-            
 def get_match(string, pattern):
-    pattern_keys = ['x', 'y']
-    counts_raw = dict(Counter(pattern))
-    counts = {key: counts_raw[key] if key in counts_raw else 0 for key in pattern_keys}
-    list_all_substrings = [string[i:j] for i in range(len(string)) for j in range(i + 1, len(string) + 1)]
-    counts_sub = dict(Counter(list_all_substrings))
-    pair_match={}
-    for x, y in product(set(list_all_substrings), repeat=2):
-        pair_to_check = {'x': x if counts['x'] and counts_sub[x] == counts['x'] else '-',
-                                 'y': y if counts['y'] and counts_sub[y] == counts['y' ] else '-'}
-        collect_string = pattern.replace('x', pair_to_check['x']).replace('y', pair_to_check['y'])
-        if collect_string == string:
-            pair_match = pair_to_check
-            break
-    
-    return [value.replace('-', '') for key,value in pair_match.items()] if pair_match else []
+    counts = Counter(pattern)
+    count_x, count_y = counts.get('x', 0), counts.get('y', 0)
+    max_len_x = len(string)//count_x if count_x else 0
+    print(max_len_x)
+    for len_x in range(0, max_len_x+1):
+        print(len_x)
+        remaining_length = len(string) - len_x *count_x
+        len_y = remaining_length // count_y if count_y and not remaining_length % count_y else 0
+        print(len_y)
+        pos = 0
+        sub_x, sub_y = '', ''
+        for char in pattern:
+            print(char)
+            print(len_x)
+            if char == 'x' and len_x:
+                sub_x = string[pos:pos+len_x]
+                print(sub_x)
+                if string[pos:pos+len_x] != sub_x:
+                    break 
+                pos += len_x
+            elif char == 'y' and len_y:
+                sub_y = string[pos:pos+len_y]
+                if string[pos:pos+len_y] != sub_y:
+                    break  
+                pos += len_y
+            else:
+                continue
+        else:
+            return [sub_x, sub_y]  # Found a match!
 
+    return []
 
-print(get_match(string, pattern))
+num = 10
+string = 'go'*num
+pattern = 'x'*num
+print(get_match(string,pattern))
