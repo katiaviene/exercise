133db34dc767721830f4c41c7c440b1792e93868
katiaviene <katiacookiefairy@gmail.com>
2025-04-04 18:48:51 +0300

    better

--- main.py
+++ main.py
@@ -3,37 +3,51 @@ from collections import Counter
 def get_match(string, pattern):
     counts = Counter(pattern)
     count_x, count_y = counts.get('x', 0), counts.get('y', 0)
-    max_len_x = len(string)//count_x if count_x else 0
-    print(max_len_x)
-    for len_x in range(0, max_len_x+1):
-        print(len_x)
-        remaining_length = len(string) - len_x *count_x
-        len_y = remaining_length // count_y if count_y and not remaining_length % count_y else 0
-        print(len_y)
-        pos = 0
-        sub_x, sub_y = '', ''
+  
+    if count_x == 0:
+        substr = string[0:int(len(string) // count_y)] 
+        result =  ["", substr if string == substr * count_y else ""]
+        return result if result[1] != "" else []
+    if count_y == 0:
+        substr = string[0:int(len(string) // count_x)] 
+        result =  [substr if string == substr * count_x else "", ""] 
+        return  result if result[0] != "" else []
+
+    max_len_x = len(string) // count_x
+
+    for len_x in range(1, max_len_x + 1):
+        remaining_length = len(string) - len_x * count_x
+        
+        if remaining_length % count_y != 0:
+            continue
+        
+        len_y = remaining_length // count_y
+        idx = 0
+        sub_x, sub_y = "", ""
+        
         for char in pattern:
-            print(char)
-            print(len_x)
-            if char == 'x' and len_x:
-                sub_x = string[pos:pos+len_x]
-                print(sub_x)
-                if string[pos:pos+len_x] != sub_x:
-                    break 
-                pos += len_x
-            elif char == 'y' and len_y:
-                sub_y = string[pos:pos+len_y]
-                if string[pos:pos+len_y] != sub_y:
-                    break  
-                pos += len_y
-            else:
-                continue
+            if char == 'x':
+                substring = string[idx:idx + len_x]
+                if not sub_x:
+                    sub_x = substring  
+                elif sub_x != substring: 
+                    break
+                idx += len_x
+            
+            elif char == 'y':
+                substring = string[idx:idx + len_y]
+                if not sub_y:
+                    sub_y = substring 
+                elif sub_y != substring:
+                    break
+                idx += len_y
         else:
-            return [sub_x, sub_y]  # Found a match!
+            return [sub_x, sub_y]
 
     return []
 
-num = 10
-string = 'go'*num
-pattern = 'x'*num
-print(get_match(string,pattern))
+
+num = 10000000
+string = 'go' * num + 'bu'*num
+pattern = 'x' * num + 'y'* num
+print(get_match(string, pattern))
